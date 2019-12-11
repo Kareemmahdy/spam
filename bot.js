0c93b5b3f4928c377b8655f2ad707a926cd75d1e
Kareemmahdy <58777994+Kareemmahdy@users.noreply.github.com>
2019-12-11 15:59:27 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("654313267979878423")
+setInterval(function() {
+channel.send(`Robot is the best`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
